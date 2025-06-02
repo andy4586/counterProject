83107d5f5a40c2b0147d0332b8e2b44c56e1ff9d
Amit Yadav <amyd@gmail.com>
2025-06-02 14:57:20 +0530

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import  {useState} from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 5
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+
+  const substractValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <button
+      onClick={substractValue}
+      >Subtract Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
